45598b0b60b2cdd7a245058360e324adbfe7f649
K-S-VICKY <vigneshvigneshks3@gmail.com>
2026-01-24 13:51:37 +0530

    updated

--- smart-street-frontend/dev-dist/sw.js
+++ smart-street-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fmi2m3n7i18"
+    "revision": "0.dt21o3sltsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
